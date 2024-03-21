a528a8317851d3c748ddfb1200b05dbea534c15a
denys58 <164342019+denys58@users.noreply.github.com>
2024-03-21 14:34:42 +0300

    Add files via upload
    first commit

---
+++ tax.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Enter employee name: "
+read name
+
+echo "Enter hours worked: "
+read hours
+
+echo "Enter rate per hour: "
+read rate
+
+basic_pay=$(($hours * $rate))
+
+if (( $basic_pay > 70000 )); then
+tax=$(($basic_pay * 0.25))
+elif (( $basic_pay >= 15000 && $basic_pay <= 70000 )); then
+tax=$(($basic_pay * 0.15))
+else
+tax=0
+fi
+net_pay=$(($basic_pay - $tax))
+
+echo "Employee: $name"
+echo "Hours worked: $hours"
+echo "Rate per hour: $rate"
+echo "Basic pay: $basic_pay"
+echo "Tax: $tax"
+echo "Net pay: $net_pay"
